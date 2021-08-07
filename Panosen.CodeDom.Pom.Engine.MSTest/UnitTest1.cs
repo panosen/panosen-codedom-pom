0b302d3e37eb736df8fab27900201f0d0f7b0ec5
harriszhang <harriszhang@live.cn>
2021-08-07 22:45:02 +0800

    update ut

--- Panosen.CodeDom.Pom.Engine.MSTest/UnitTest1.cs
+++ Panosen.CodeDom.Pom.Engine.MSTest/UnitTest1.cs
@@ -21,12 +21,14 @@ namespace Panosen.CodeDom.Pom.Engine.MSTest
 
         static string PrepareExpected()
         {
-            return @"<project xmlns=""http://maven.apache.org/POM/4.0.0"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"">
+            return @"<project xmlns=""http://maven.apache.org/POM/4.0.0"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
+        xsi:schemaLocation=""http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"">
     <modelVersion>4.0.0</modelVersion>
 
     <groupId>abc</groupId>
     <artifactId>defr</artifactId>
     <version>1.0.0</version>
+
 </project>
 ";
         }
